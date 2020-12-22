2d7153f4f344e1fcb49b507a96cd583cd7ef2ff0
evgeny knyazkin <evgen.kudesnik@gmail.com>
2020-12-22 22:19:06 +0300

    reformat

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
+    public int calculate(int price) {
 
-    public int calculate( int price ) {
         int miles = price / 20;
         return miles;
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
+
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
